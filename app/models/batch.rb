92b6169541c44f92887e14ab56371a6989064f1f
rakesh kumar <v-rakesh.kumar@decurtis.com>
2020-01-31 16:57:53 +0530

    makeheroku

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,4 +2,13 @@ class Batch < ApplicationRecord
 
 	belongs_to :faculty
 
+
+	def name
+		"laxmi"
+	end
+
+	def self.full_name
+		"laxmi vishwakarma"
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_24_092614) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
